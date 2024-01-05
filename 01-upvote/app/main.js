19dba32813e3ca7c42e312f57499027000263275
ritatanght <doggymint@gmail.com>
2024-01-04 21:09:28 -0500

    Add sortedSubmissions as computed property

--- 01-upvote/app/main.js
+++ 01-upvote/app/main.js
@@ -3,6 +3,11 @@ const upvoteApp = {
   data() {
     return { submissions: Seed.submissions };
   },
+  computed: {
+    sortedSubmissions() {
+      return this.submissions.sort((a, b) => b.votes - a.votes);
+    },
+  },
 };
 
 Vue.createApp(upvoteApp).mount("#app");
