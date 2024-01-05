de66f41081725b741aade8d2b7588555a1cc70e6
ritatanght <doggymint@gmail.com>
2024-01-04 21:22:20 -0500

    Register the submission component to root app

--- 01-upvote/app/main.js
+++ 01-upvote/app/main.js
@@ -1,3 +1,36 @@
+const submissionComponent = {
+  template: `
+      <figure class="media-left">
+        <img class="image is-64x64" :src="submission.submissionImage" />
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>
+              <a :href="submission.url" class="has-text-info"
+                >{{submission.title}}</a
+              >
+              <span class="tag is-small">#{{submission.id}}</span>
+            </strong>
+            <br />
+            {{submission.description}}
+            <br />
+            <small class="is-size-7">
+              Submitted by:
+              <img class="image is-24x24" :src="submission.avatar" />
+            </small>
+          </p>
+        </div>
+      </div>
+      <div class="media-right">
+        <span class="icon is-small" @click="upvote(submission.id)">
+          <i class="fa fa-chevron-up"></i>
+          <strong class="has-text-info">{{submission.votes}}</strong>
+        </span>
+      </div>
+  `,
+};
+
 // instance options
 const upvoteApp = {
   data() {
@@ -15,6 +48,9 @@ const upvoteApp = {
       );
       submission.votes++;
     },
+  },
+  components: {
+    "submission-component": submissionComponent,
   },
 };
 
