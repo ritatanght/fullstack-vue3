2bacead8cf2782a7c0d9eaf01c70c24f2beff706
ritatanght <doggymint@gmail.com>
2024-01-07 18:41:59 -0500

    Transfer corresponding html and styles to CalendarEvent

---
+++ 02-calendar_app/src/app/components/CalendarEvent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="day-event" style="background-color: rgb(153, 255, 153)">
+    <div>
+      <span class="has-text-centered details">{{ event.details }}</span>
+      <div class="has-text-centered icons">
+        <i class="fa fa-pencil-square edit-icon"></i>
+        <i class="fa fa-trash-o delete-icon"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CalendarEvent",
+  props: ["day", "event"],
+};
+</script>
+
+<style lang="scss" scoped>
+.day-event {
+  margin-top: 6px;
+  margin-bottom: 6px;
+  display: block;
+  color: #4c4c4c;
+  padding: 5px;
+  .details {
+    display: block;
+  }
+  .icons .fa {
+    padding: 0 2px;
+  }
+  input {
+    background: none;
+    border: 0;
+    border-bottom: 1px solid #fff;
+    width: 100%;
+    &:focus {
+      outline: none;
+    }
+  }
+}
+</style>
