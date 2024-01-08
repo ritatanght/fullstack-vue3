fc3a3db57bfdf6e4aad5bd509af7b12ffa807281
ritatanght <doggymint@gmail.com>
2024-01-08 12:20:02 -0500

    Add getActiveDay and setActiveDay methods to store

--- 02-calendar_app/src/app/store.js
+++ 02-calendar_app/src/app/store.js
@@ -10,4 +10,12 @@ export const store = {
   state: {
     data: reactive(seedData),
   },
+  getActiveDay() {
+    return this.state.data.find((day) => day.active);
+  },
+  setActiveDay(dayId) {
+    this.state.data.map((dayObj) =>
+      dayObj.id === dayId ? (dayObj.active = true) : (dayObj.active = false)
+    );
+  },
 };
