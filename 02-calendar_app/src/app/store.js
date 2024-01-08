ea05e170ab805bab6afe1927248db463cd0c461a
ritatanght <doggymint@gmail.com>
2024-01-07 21:42:43 -0500

    Declare reactive state

--- 02-calendar_app/src/app/store.js
+++ 02-calendar_app/src/app/store.js
@@ -1,6 +1,13 @@
+import { reactive } from "vue";
 import { seedData } from "./seed";
+
+// When we return an object from the data() in a component,
+// Vue internally makes the data reactive.
+
+// When we’re constructing this store outside of a component,
+// we can use the reactive() method to ensure that the view updates whenever state changes.
 export const store = {
   state: {
-    data: seedData,
+    data: reactive(seedData),
   },
 };
