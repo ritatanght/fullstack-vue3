48f254c24fdc5531b0d500f2b4f2aae8438d39d8
ritatanght <doggymint@gmail.com>
2024-01-08 22:55:24 -0500

    Add html to display conditional on the edit value

--- 02-calendar_app/src/app/components/CalendarEvent.vue
+++ 02-calendar_app/src/app/components/CalendarEvent.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="day-event" :style="getEventBackgroundColor">
-    <div>
+    <div v-if="!event.edit">
       <span class="has-text-centered details">{{ event.details }}</span>
       <div class="has-text-centered icons">
         <i class="fa fa-pencil-square edit-icon"></i>
         <i class="fa fa-trash-o delete-icon"></i>
+      </div>
+    </div>
+    <div v-else>
+      <input :placeholder="event.details" />
+      <div class="has-text-centered icons">
+        <i class="fa fa-check"></i>
       </div>
     </div>
   </div>
