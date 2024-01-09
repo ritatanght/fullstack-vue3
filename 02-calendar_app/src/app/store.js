fa1733f8267b85f52bc660ccb89dc742b952e2af
ritatanght <doggymint@gmail.com>
2024-01-08 23:08:38 -0500

    Add resetEditOfAllEvents to only allows editing a single event at a time

--- 02-calendar_app/src/app/store.js
+++ 02-calendar_app/src/app/store.js
@@ -23,11 +23,18 @@ export const store = {
     activeDay.events.push({ details: eventDetails, edit: false });
   },
   editEvent(dayId, eventDetails) {
+    this.resetEditOfAllEvents(); // this ensures only one event can be edited at a time
     const dayObj = this.state.data.find((day) => day.id === dayId);
     const eventObj = dayObj.events.find(
       (event) => event.details === eventDetails
     );
     // set the edit property for the target event to true
     eventObj.edit = true;
+  },
+  // iterate through all days and set all the events' edit property to 'false'
+  resetEditOfAllEvents() {
+    this.state.data.map((dayObj) => {
+      dayObj.events.map((event) => (event.edit = false));
+    });
   },
 };
