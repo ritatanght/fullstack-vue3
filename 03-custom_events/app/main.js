7a6ccb03f018319133f69d444dd46a5abb9b5239
Rita Tang <doggymint@gmail.com>
2024-01-13 22:41:56 -0500

    Add props to app

--- 03-custom_events/app/main.js
+++ 03-custom_events/app/main.js
@@ -1,7 +1,14 @@
 const inputComponent = {
-  template: `<input class="input is-small" type="text" />`,
+  template: `<input class="input is-small" type="text"/>`,
 };
 const app = {
+  data() {
+    return {
+      notes: [],
+      timestamps: [],
+      placeholder: "Enter a note",
+    };
+  },
   components: {
     "input-component": inputComponent,
   },
