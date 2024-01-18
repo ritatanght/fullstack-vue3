c9ba425092ca4393cbc4fa7cc23916daf897a778
ritatanght <doggymint@gmail.com>
2024-01-17 23:40:45 -0500

    Add starter template and style for two list components

--- 05-shopping_cart/src/App.vue
+++ 05-shopping_cart/src/App.vue
@@ -1,9 +1,47 @@
-<script setup>
+<script>
+import CartList from "./app/components/cart/CartList.vue";
+import ProductList from "./app/components/product/ProductList.vue";
+export default {
+  name: "App",
+  components: {
+    CartList,
+    ProductList,
+  },
+};
 </script>
 
 <template>
- <p>Vue is running</p>
+  <div id="app">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-3">
+          <CartList />
+        </div>
+        <div class="column is-9">
+          <ProductList />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style>
+html,
+body {
+  background: #f2f6fa;
+}
+
+#app {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+@media screen and (min-width: 769px) {
+  html,
+  body {
+    height: 100%;
+  }
+}
 </style>
