2438ed3af9258db20df8028831623b4250671e3a
ritatanght <doggymint@gmail.com>
2024-01-19 22:50:51 -0500

    Fix typo in path

--- 05-shopping_cart/src/app/store/index.js
+++ 05-shopping_cart/src/app/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from "vuex";
-import product from "./module/product";
-import cart from "./module/cart";
+import product from "./modules/product"
+import cart from "./modules/cart";
 
 // multiple module objects can be introduced to the store’s modules property
 export default createStore({
