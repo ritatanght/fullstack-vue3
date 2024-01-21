5685e0fbe31acdede4683e0be618a5969a52b507
ritatanght <doggymint@gmail.com>
2024-01-21 18:41:31 -0500

    Add ButtonRow component with template and basic methods

--- 06-form_handling/app/main.js
+++ 06-form_handling/app/main.js
@@ -1,1 +1,29 @@
-Vue.createApp({}).mount("#app");
+const ButtonRow = {
+  template: `
+    <div>
+      <button @click="onHoodieClick" class="ui button">Hoodie</button>
+      <button @click="onTeeClick" class="ui button">Tee</button>
+      <button @click="onFittedCapClick" class="ui button">Fitted Cap</button>
+      <button @click="onJacketClick" class="ui button">Jacket</button>
+    </div>`,
+  methods: {
+    onHoodieClick(e) {
+      console.log("The user clicked button-hoodie", e);
+    },
+    onTeeClick(e) {
+      console.log("The user clicked button-tee", e);
+    },
+    onFittedCapClick(e) {
+      console.log("The user clicked button-fitted-cap", e);
+    },
+    onJacketClick(e) {
+      console.log("The user clicked button-jacket", e);
+    },
+  },
+};
+
+const app = {
+  components: { ButtonRow },
+};
+
+Vue.createApp(app).mount("#app");
