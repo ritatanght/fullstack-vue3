ef397c984684884992d2c426c1648d5d1a9ea703
ritatanght <doggymint@gmail.com>
2024-01-20 21:51:22 -0500

    Render cart item in list

--- 05-shopping_cart/src/app/components/cart/CartList.vue
+++ 05-shopping_cart/src/app/components/cart/CartList.vue
@@ -4,48 +4,38 @@
       <i class="fa fa-2x fa-shopping-cart"></i>
     </div>
     <ul>
-      <li class="cart-item">
-        <div>
-          <p class="cart-item--title is-inline">The Fullstack Hoodie</p>
-          <div class="is-pulled-right">
-            <i class="fa fa-arrow-circle-up cart-item--modify"></i>
-            <i class="fa fa-arrow-circle-down cart-item--modify"></i>
-          </div>
-          <div class="cart-item--content">
-            <span
-              class="cart-item--price has-text-primary has-text-weight-bold"
-            >
-              19.99$ each
-            </span>
-            <span class="cart-item--quantity has-text-grey is-pulled-right">
-              Quantity: 2
-            </span>
-          </div>
-        </div>
+      <li v-for="cartItem in cartItems" :key="cartItem" class="cart-item">
+        <CartListItem :cartItem="cartItem" />
       </li>
       <div class="cart-details">
-        <p>Total Quantity: <span class="has-text-weight-bold">2</span></p>
+        <p>
+          Total Quantity:
+          <span class="has-text-weight-bold">{{ cartQuantity }}</span>
+        </p>
         <p class="cart-remove-all--text">
           <i class="fa fa-trash"></i>Remove all
         </p>
       </div>
     </ul>
     <button class="button is-primary">
-      Checkout (<span class="has-text-weight-bold">$</span>)
+      Checkout (<span class="has-text-weight-bold">${{ cartTotal }}</span
+      >)
     </button>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import CartListItem from "./CartListItem.vue";
 export default {
   name: "CartList",
   created() {
     this.$store.dispatch("getCartItems");
   },
   computed: {
-    ...mapGetters(["cartItems, cartTotal, cartQuantity"]),
+    ...mapGetters(["cartItems", "cartTotal", "cartQuantity"]),
   },
+  components: { CartListItem },
 };
 </script>
 
@@ -86,7 +76,4 @@ export default {
   padding-right: 5px;
 }
 
-.cart-item--modify {
-  cursor: pointer;
-}
 </style>
