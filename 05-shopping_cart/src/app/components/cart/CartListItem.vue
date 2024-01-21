aae8b123d5b90203471492e84b9d4968c9279f39
ritatanght <doggymint@gmail.com>
2024-01-20 22:55:48 -0500

    Add actions to add or remove cart items within cart

--- 05-shopping_cart/src/app/components/cart/CartListItem.vue
+++ 05-shopping_cart/src/app/components/cart/CartListItem.vue
@@ -2,8 +2,14 @@
   <div>
     <p class="cart-item--title is-inline">{{ cartItem.title }}</p>
     <div class="is-pulled-right">
-      <i class="fa fa-arrow-circle-up cart-item--modify"></i>
-      <i class="fa fa-arrow-circle-down cart-item--modify"></i>
+      <i
+        class="fa fa-arrow-circle-up cart-item--modify"
+        @click="addCartItem(cartItem)"
+      ></i>
+      <i
+        class="fa fa-arrow-circle-down cart-item--modify"
+        @click="removeCartItem(cartItem)"
+      ></i>
     </div>
     <div class="cart-item--content">
       <span class="cart-item--price has-text-primary has-text-weight-bold">
@@ -17,9 +23,13 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   name: "CartListItem",
   props: ["cartItem"],
+  methods: {
+    ...mapActions(["addCartItem", "removeCartItem"]),
+  },
 };
 </script>
 
