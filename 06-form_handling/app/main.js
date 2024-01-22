622569a42a0bc551b0d9fda67097aa873b1ae41b
ritatanght <doggymint@gmail.com>
2024-01-21 21:07:48 -0500

    Add names and values to buttons and replace multiple methods with one

--- 06-form_handling/app/main.js
+++ 06-form_handling/app/main.js
@@ -1,23 +1,15 @@
 const ButtonRow = {
   template: `
     <div>
-      <button @click="onHoodieClick" class="ui button">Hoodie</button>
-      <button @click="onTeeClick" class="ui button">Tee</button>
-      <button @click="onFittedCapClick" class="ui button">Fitted Cap</button>
-      <button @click="onJacketClick" class="ui button">Jacket</button>
+      <button @click="onButtonClick" name="button-hoodie" value="fullstack-hoodie" class="ui button">Hoodie</button>
+      <button @click="onButtonClick" name="button-tee" value="fullstack-tee" class="ui button">Tee</button>
+      <button @click="onButtonClick" name="button-fitted-cap" value="fullstack-fitted-cap" class="ui button">Fitted Cap</button>
+      <button @click="onButtonClick" name="button-jacket" value="fullstack-jacket" class="ui button">Jacket</button>
     </div>`,
   methods: {
-    onHoodieClick(e) {
-      console.log("The user clicked button-hoodie", e);
-    },
-    onTeeClick(e) {
-      console.log("The user clicked button-tee", e);
-    },
-    onFittedCapClick(e) {
-      console.log("The user clicked button-fitted-cap", e);
-    },
-    onJacketClick(e) {
-      console.log("The user clicked button-jacket", e);
+    onButtonClick(event) {
+      const button = event.target;
+      console.log(`The user clicked ${button.name}: ${button.value}`);
     },
   },
 };
