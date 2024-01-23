e109c61ce23b18c535cc27b983bd62abb4075166
ritatanght <doggymint@gmail.com>
2024-01-23 00:12:45 -0500

    Add blank store

---
+++ 06-form_handling/vuex_form/store.js
@@ -0,0 +1,15 @@
+const state = {};
+
+const mutations = {};
+
+const actions = {};
+
+const getters = {};
+
+// globalize the store with window.store for it to be accessed elsewhere
+window.store = Vuex.createStore({
+  state,
+  mutations,
+  actions,
+  getters,
+});
