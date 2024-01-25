6b2b11444973560c8c98e9eaa71d05f291f825d4
ritatanght <doggymint@gmail.com>
2024-01-25 16:13:32 -0500

    Add router-view to app and add styles

--- 07-routing/src/App.vue
+++ 07-routing/src/App.vue
@@ -1,25 +1,14 @@
 <script>
-import CartList from "./app/components/cart/CartList.vue";
-import ProductList from "./app/components/product/ProductList.vue";
 export default {
   name: "App",
-  components: {
-    CartList,
-    ProductList,
-  },
 };
 </script>
 
 <template>
-  <div id="app">
-    <div class="container">
-      <div class="columns">
-        <div class="column is-3">
-          <CartList />
-        </div>
-        <div class="column is-9">
-          <ProductList />
-        </div>
+  <div class="container">
+    <div class="columns">
+      <div class="column is-6 column--align-center">
+        <router-view></router-view>
       </div>
     </div>
   </div>
@@ -32,16 +21,32 @@ body {
 }
 
 #app {
-  height: 100%;
+  min-height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
-@media screen and (min-width: 769px) {
-  html,
-  body {
-    height: 100%;
-  }
+.container {
+  width: 100%;
+}
+
+.column--align-center {
+  margin: 0 auto;
+}
+
+.navigation-buttons {
+  position: absolute;
+  top: 5px;
+  width: 99%;
+  z-index: 99;
+}
+
+.navigation-buttons .button {
+  margin: 0 2px;
+}
+
+.navigation-buttons .button .fa {
+  padding-right: 5px;
 }
 </style>
