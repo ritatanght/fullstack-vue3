861da070d53c9144ba8b53941865e3fb860f5857
ritatanght <doggymint@gmail.com>
2024-01-25 18:00:32 -0500

    Revise template and styles for cart item

--- 07-routing/src/app/components/cart/CartListItem.vue
+++ 07-routing/src/app/components/cart/CartListItem.vue
@@ -1,23 +1,25 @@
 <template>
-  <div>
-    <p class="cart-item--title is-inline">{{ cartItem.title }}</p>
-    <div class="is-pulled-right">
-      <i
-        class="fa fa-arrow-circle-up cart-item--modify"
-        @click="addCartItem(cartItem)"
-      ></i>
-      <i
-        class="fa fa-arrow-circle-down cart-item--modify"
-        @click="removeCartItem(cartItem)"
-      ></i>
+  <div class="box">
+    <div class="cart-item__details">
+      <p class="is-inline">{{ cartItem.title }}</p>
+      <div>
+        <span class="cart-item--price has-text-primary has-text-weight-bold">
+          {{ cartItem.price }}$ x {{ cartItem.quantity }}
+        </span>
+        <span>
+          <i
+            @click="addCartItem(cartItem)"
+            class="fa fa-arrow-circle-up cart-item__modify"
+          ></i>
+          <i
+            @click="removeCartItem(cartItem)"
+            class="fa fa-arrow-circle-down cart-item__modify"
+          ></i>
+        </span>
+      </div>
     </div>
-    <div class="cart-item--content">
-      <span class="cart-item--price has-text-primary has-text-weight-bold">
-        {{ cartItem.price }}$ each
-      </span>
-      <span class="cart-item--quantity has-text-grey is-pulled-right">
-        Quantity: {{ cartItem.quantity }}
-      </span>
+    <div class="cart-item__image">
+      <img :src="`/assets/${cartItem.image_tag}`" />
     </div>
   </div>
 </template>
@@ -34,6 +36,22 @@ export default {
 </script>
 
 <style scoped>
+.box {
+  height: 90px;
+  padding: 10px;
+}
+
+.cart-item__details {
+  float: left;
+  width: 250px;
+  padding: 10px;
+}
+
+.cart-item__image img {
+  float: right;
+  height: 70px;
+}
+
 .cart-item--modify {
   cursor: pointer;
   margin: 0 1px;
