3cb449ee9f0f49cabbef289687d33c7772923ef4
ritatanght <doggymint@gmail.com>
2024-01-25 15:54:28 -0500

    Add NotFound for route that doesn’t map to any of the component

--- 07-routing/src/app/router/index.js
+++ 07-routing/src/app/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import CartList from "../components/cart/CartList.vue";
 import ProductList from "../components/product/ProductList.vue";
+import NotFound from "../components/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(), // in HTML5 history mode, (no hashes in URL)
@@ -11,5 +12,8 @@ const router = createRouter({
     },
     { path: "/cart", component: CartList },
     { path: "/", redirect: "/products" },
+    { path: "/:pathMatch(.*)*", component: NotFound },
   ],
 });
+
+export default router;
