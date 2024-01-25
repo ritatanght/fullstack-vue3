c63982ee44a511c2f8fbb3ab05367ec9dc0cfa66
Rita Tang <doggymint@gmail.com>
2024-01-25 15:14:14 -0500

    Add basic router setup template

---
+++ 07-routing/src/app/router/index.js
@@ -0,0 +1,6 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(), // in HTML5 history mode, (no hashes in URL)
+  routes: [],
+});
