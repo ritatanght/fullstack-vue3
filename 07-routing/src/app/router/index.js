5478b62d9ab6bf8e85ffbdbc1496bf17d08e463e
ritatanght <doggymint@gmail.com>
2024-01-25 18:08:23 -0500

    Add route for ProductItem

--- 07-routing/src/app/router/index.js
+++ 07-routing/src/app/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import CartList from "../components/cart/CartList.vue";
 import ProductList from "../components/product/ProductList.vue";
 import NotFound from "../components/NotFound.vue";
+import ProductItem from "../components/product/ProductItem.vue";
 
 const router = createRouter({
   history: createWebHistory(), // in HTML5 history mode, (no hashes in URL)
@@ -9,6 +10,11 @@ const router = createRouter({
     {
       path: "/products",
       component: ProductList,
+    },
+    {
+      path: "/products/:id",
+      component: ProductItem,
+      props: true, // enable props, so we can access id in ProductItem
     },
     { path: "/cart", component: CartList },
     { path: "/", redirect: "/products" },
