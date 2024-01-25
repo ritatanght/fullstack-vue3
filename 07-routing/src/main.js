4ad02da5023cbf3f71e6c69e5ef15cbd80b96726
ritatanght <doggymint@gmail.com>
2024-01-25 15:56:55 -0500

    Inject the router to app

--- 07-routing/src/main.js
+++ 07-routing/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./app/store";
+import router from "./app/router";
 
 createApp(App)
+  .use(router)
   .use(store)
   .mount("#app");
