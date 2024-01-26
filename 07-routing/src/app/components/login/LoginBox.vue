e8f20f3e43fcc7508cc0a6e2e56311ab9a30313a
ritatanght <doggymint@gmail.com>
2024-01-25 21:08:23 -0500

    Add login method to button and redirect to /products after successful login

--- 07-routing/src/app/components/login/LoginBox.vue
+++ 07-routing/src/app/components/login/LoginBox.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="login" class="box has-text-centered">
     <h2 class="title">Fullstack Clothing</h2>
-    <button class="button is-primary">Login</button>
+    <button @click="login" class="button is-primary">Login</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "LoginBox",
+  methods: {
+    login() {
+      this.$store.dispatch("login").then(() => this.$router.push("/products"));
+    },
+  },
 };
 </script>
 
