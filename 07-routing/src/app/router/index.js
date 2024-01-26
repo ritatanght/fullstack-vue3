4dc486e2f654e9dc75333c276143bcb84834e211
ritatanght <doggymint@gmail.com>
2024-01-26 13:54:02 -0500

    Add global guard and per-route guard

--- 07-routing/src/app/router/index.js
+++ 07-routing/src/app/router/index.js
@@ -16,12 +16,36 @@ const router = createRouter({
       path: "/products/:id",
       component: ProductItem,
       props: true, // enable props, so we can access id in ProductItem
+      beforeEnter: (to, from, next) => {
+        const id = to.params.id;
+        if (![1, 2, 3, 4].includes(Number(id))) next("/not-found");
+        else next();
+      },
     },
     { path: "/cart", component: CartList },
     { path: "/", redirect: "/products" },
-    { path: "/login", component: LoginBox },
+    {
+      path: "/login",
+      component: LoginBox,
+      beforeEnter: (to, from, next) => {
+        const token = localStorage.getItem("token");
+        if (token) next("/products");
+        else next();
+      },
+    },
     { path: "/:pathMatch(.*)*", component: NotFound },
   ],
+});
+
+/* global route guard
+1. to - the target route object.
+2. from - the current route object.
+3. next() - the function that must be invoked to complete routing the user */
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem("token");
+  // forces the user to the /login route.
+  if (!token && to.path !== "/login") next("/login");
+  else next();
 });
 
 export default router;
