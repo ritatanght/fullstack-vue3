8efe85022d31631bca2c2065b927fc72f9d7e20a
ritatanght <doggymint@gmail.com>
2024-01-25 19:01:13 -0500

    Add loginModule and include in store

--- 07-routing/src/app/store/index.js
+++ 07-routing/src/app/store/index.js
@@ -1,11 +1,13 @@
 import { createStore } from "vuex";
-import product from "./modules/product"
+import product from "./modules/product";
 import cart from "./modules/cart";
+import login from "./modules/login";
 
 // multiple module objects can be introduced to the store’s modules property
 export default createStore({
   modules: {
     product,
     cart,
+    login,
   },
 });
---
+++ 07-routing/src/app/store/modules/login/index.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const state = {};
+const mutations = {};
+const actions = {};
+const getters = {};
+
+const loginModule = {
+  state,
+  mutations,
+  actions,
+  getters,
+};
+
+export default loginModule;
