1ee1a3a4a3888c967e1b00498fe575a3ad91686e
ritatanght <doggymint@gmail.com>
2024-01-25 20:56:29 -0500

    Add basic scaffold for LoginBox

---
+++ 07-routing/src/app/components/login/LoginBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="login" class="box has-text-centered">
+    <h2 class="title">Fullstack Clothing</h2>
+    <button class="button is-primary">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginBox",
+};
+</script>
+
+<style scoped>
+.box {
+  padding: 30px;
+}
+</style>
