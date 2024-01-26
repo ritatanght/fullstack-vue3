37201c0ce0557ec6cfcb81054b8fc8bd8d728210
ritatanght <doggymint@gmail.com>
2024-01-25 20:44:03 -0500

    Add state and methods to loginModule

--- 07-routing/src/app/store/modules/login/index.js
+++ 07-routing/src/app/store/modules/login/index.js
@@ -1,9 +1,34 @@
 import axios from "axios";
 
-const state = {};
-const mutations = {};
-const actions = {};
-const getters = {};
+const state = {
+  token: null,
+};
+
+const mutations = {
+  SET_TOKEN(state, token) {
+    state.token = token;
+  },
+};
+
+const actions = {
+  login({ commit }) {
+    axios.post("/api/login").then((res) => {
+      localStorage.setItem("token", res.data.token);
+      commit("SET_TOKEN", res.data.token);
+    });
+  },
+  logout({ commit }) {
+    return new Promise((resolve, reject) => {
+      localStorage.removeItem("token");
+      commit("SET_TOKEN", null);
+      resolve();
+    });
+  },
+};
+
+const getters = {
+  token: (state) => state.token,
+};
 
 const loginModule = {
   state,
